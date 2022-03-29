eadc196ba956bce8569ed5324bcc59afca12868b
Marcin Kozicki <m.kozicki2@gmail.com>
2022-03-29 17:43:19 +0200

    Remove build folder

--- build/api/models/productsModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var mongoose = require('mongoose');
-var productShema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    productImage: { type: String, required: true }
-});
-module.exports = mongoose.model('Product', productShema);
--- build/api/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var express = require('express');
-var UsersController = require('../controllers/UsersController');
-var router = express.Router();
-router.post('/signup', UsersController.createUser);
-router.post('/login', UsersController.loginUser);
-router.delete('/delete/:userId', UsersController.deleteUser);
-module.exports = router;
