059c2a7b5805ec9cae21510051c78f471ba30bf0
Marcin Kozicki <m.kozicki2@gmail.com>
2023-03-08 20:45:39 +0100

    Mongo db ts conversion

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,12 @@ import MongoDatabase from './infra/db/mongo';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { version } = require('../package.json');
 
+const { log } = console;
+
 const port = process.env.PORT || 3000;
 
 const server = new ExpressServer().create(version);
 MongoDatabase.initConnection();
 server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log('Server running on port: http://localhost:' + port);
+  log('Server running on port: http://localhost:' + port);
 });
