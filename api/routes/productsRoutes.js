eec24f10f0382fe12ac7a079b8ba60838ab3eb7a
Marcin Kozicki <m.kozicki2@gmail.com>
2024-07-11 23:33:40 +0200

    Update packages

--- api/routes/productsRoutes.js
+++ api/routes/productsRoutes.js
@@ -4,21 +4,9 @@ const router = express.Router();
 
 const mongsoose = require('mongoose');
 
-const multer = require('multer');
-
 const ProductsController = require('../controllers/ProductsController');
 
 const checkAuth = require('../auth/check-auth');
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/images');
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, `${uniqueSuffix} ${file.originalname}`);
-  },
-});
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
@@ -28,19 +16,9 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const upload = multer({
-  storage: storage,
-  fileFilter: fileFilter,
-  limits: {
-    fieldSize: 1024 * 1024,
-  },
-});
-
 router.get('/', ProductsController.getAllProducts);
 
 router.get('/:productId', ProductsController.getProductById);
-
-router.post('/', checkAuth, upload.single('productImage'), ProductsController.createdProduct);
 
 router.patch('/:productId', checkAuth, ProductsController.updateProduct);
 
